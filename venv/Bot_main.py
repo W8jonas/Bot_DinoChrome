e561ba47404f3858edc0fa42319fd817d053e17a
w8jonas <jonashenrique989@gmail.com>
2019-02-18 00:11:30 -0300

    first commit

---
+++ venv/Bot_main.py
@@ -0,0 +1,31 @@
+import time
+
+from PIL import ImageGrab
+import pyautogui
+
+X = 300.0  # X2 = X + 90
+Y1 = 380
+Y2 = 470
+
+def print_tela():
+    screen = ImageGrab.grab()
+    return screen
+
+def ler_tela(screen):
+    aux_color = screen.getpixel((int(X), Y1))
+    for x in range(int(X), int(X+90)):
+        for y in range(Y1, Y2):
+            color = screen.getpixel((x, y))
+            if color != aux_color:
+                return True
+            else:
+                aux_color = color
+
+print("Iniciando em 3 segundos")
+time.sleep(3)
+
+while True:
+    screen = print_tela()
+    if ler_tela(screen):
+        pyautogui.press("space")
+        X += 0.5
